51d9ac262aa578a62b0314897bdce5b0e4fa1b3c
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2023-09-25 16:07:59 +0530

    sidenavbar basic structure

--- src/components/main/MainSideNav.jsx
+++ src/components/main/MainSideNav.jsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
+import {
+  AreaChartOutlined,
+  DollarOutlined,
+  PaperClipOutlined,
+  WalletOutlined,
+} from "@ant-design/icons";
+
+const menuItems = [
+  {
+    key: "dashboard",
+    label: <Link to="/dashboard">Dashboard</Link>,
+    icon: <AreaChartOutlined />,
+  },
+  {
+    key: "expense",
+    label: <Link to="/expense">Expense</Link>,
+    icon: <WalletOutlined />,
+  },
+  {
+    key: "income",
+    label: <Link to="/income">Income</Link>,
+    icon: <DollarOutlined />,
+  },
+  {
+    key: "report",
+    label: <Link to="/report">Report</Link>,
+    icon: <PaperClipOutlined />,
+  },
+];
 const MainSideNav = () => {
-  return <div>sidenav</div>;
+  return (
+    <div>
+      <Menu
+        style={{
+          width: 300,
+        }}
+        items={menuItems}
+        mode="inline"
+      />
+    </div>
+  );
 };
 
 export default MainSideNav;
