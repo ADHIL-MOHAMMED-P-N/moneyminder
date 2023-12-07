ab9e6f9c21bd2d1992d0226f9a43ca75a82a37a8
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2023-12-07 23:54:08 +0530

    404 page icon,button

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,7 +1,34 @@
 import React from "react";
-
+import ErrorImage from "../components/utils/ErrorImage";
+import { useNavigate } from "react-router-dom";
+import { Button, Typography } from "antd";
+const { Title } = Typography;
 const Error404 = () => {
-  return <div>Error404 resource not found</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={{ width: 300, textAlign: "center" }}>
+        <ErrorImage />
+        <Title style={{ textAlign: "center" }} level={3}>
+          404 Resource not found
+        </Title>
+        <Button
+          style={{ margin: "auto" }}
+          onClick={() => navigate("/dashboard")}
+        >
+          Home
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Error404;
