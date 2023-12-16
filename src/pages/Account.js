af52160b23209b8d0349960d2372fc3f255e8f74
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2023-12-16 16:26:31 +0530

    account page ui, data integration

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,16 +1,23 @@
 import { Avatar, Card, Col, Row, Typography } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { SettingOutlined, UserOutlined } from "@ant-design/icons";
 import { useUserAuth } from "../context/UserAuthContext";
+import create from "@ant-design/icons/lib/components/IconFont";
 
 const { Title } = Typography;
 const Account = () => {
   const { user } = useUserAuth();
   console.log(user);
-  const createdAt = new Date(user.metadata.creationTime);
+  const createdAt = user.metadata ? new Date(user.metadata.creationTime) : null;
+  const paraStyle = {
+    fontWeight: 400,
+    fontSize: 12,
+    color: "gray",
+    margin: 0,
+  };
 
   return (
     <div>
-      <Row>
+      <Row gutter={20}>
         <Col span={8}>
           <Card style={{ borderRadius: 0 }} size="small">
             <div
@@ -23,31 +30,73 @@ const Account = () => {
               <Avatar size={60} src={user.photoURL} icon={<UserOutlined />} />
               <Title
                 style={{
-                  fontWeight: 400,
+                  fontWeight: 500,
                   fontSize: 15,
                   margin: 0,
                   marginTop: 10,
                 }}
                 level={5}
               >
-                {user.displayName || "User"}
+                {user.displayName ? user.displayName : "User"}
               </Title>
-              <p
-                style={{
-                  fontWeight: 400,
-                  fontSize: 10,
-                  color: "gray",
-                  margin: 0,
-                }}
-                level={5}
-              >
-                Member since {createdAt.getDate()}/{createdAt.getMonth() + 1}/
-                {createdAt.getFullYear()}
+              <p style={paraStyle}>
+                Member since
+                {createdAt
+                  ? ` ${createdAt.getDate()}/${createdAt.getMonth() + 1}/
+                ${createdAt.getFullYear()}`
+                  : "___"}
               </p>
             </div>
           </Card>
         </Col>
-        <Col span={16}></Col>
+        <Col span={16}>
+          <Card
+            className="account-detailsCard"
+            style={{ borderRadius: 0 }}
+            size="small"
+            extra={<SettingOutlined />}
+            title={<p style={paraStyle}>Account Details</p>}
+          >
+            <Row>
+              <Col span={12}>
+                <div style={{ marginBottom: 20 }}>
+                  <p style={{ ...paraStyle, color: "blacks", fontWeight: 500 }}>
+                    First Name
+                  </p>
+                  <p style={paraStyle}>
+                    {user.displayName ? user.displayName.split(" ")[0] : "___"}
+                  </p>
+                </div>
+                <div>
+                  <p style={{ ...paraStyle, color: "blacks", fontWeight: 500 }}>
+                    Email
+                  </p>
+                  <p style={paraStyle}>{user.email ? user.email : "___"}</p>
+                </div>
+              </Col>
+              <Col span={12}>
+                <div style={{ marginBottom: 20 }}>
+                  <p style={{ ...paraStyle, color: "blacks", fontWeight: 500 }}>
+                    Last Name
+                  </p>
+                  <p style={paraStyle}>
+                    {user.displayName
+                      ? user.displayName.split(" ").slice(1).join(" ")
+                      : "___"}
+                  </p>
+                </div>
+                <div>
+                  <p style={{ ...paraStyle, color: "blacks", fontWeight: 500 }}>
+                    Phone
+                  </p>
+                  <p style={paraStyle}>
+                    {user.phoneNumber ? user.phoneNumber : "___"}
+                  </p>
+                </div>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
       </Row>
     </div>
   );
